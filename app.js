209a5376fe55e936291f7f4d6a91df8e431a11fe
Your Name <you@example.com>
2021-05-20 01:01:31 +0530

    update port

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ Post.find({_id:requestedId},function(err,post){
 
 });
 
-app.listen(3000, function() {
+app.listen(proces.env.PORT||3000, function() {
     console.log("Server started on port 3000");
 });
